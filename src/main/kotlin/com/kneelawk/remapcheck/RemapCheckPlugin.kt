3cbf7ba8dce51561943257cf6806415fa903321a
Kneelawk <kneelawk@gmail.com>
2024-05-30 03:03:07 -0700

    Move publishing to CI & use vineflower

--- src/main/kotlin/com/kneelawk/remapcheck/RemapCheckPlugin.kt
+++ src/main/kotlin/com/kneelawk/remapcheck/RemapCheckPlugin.kt
@@ -7,5 +7,7 @@ import org.gradle.kotlin.dsl.create
 class RemapCheckPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.extensions.create("remapCheck", RemapCheckExtension::class, project)
+        
+        project.tasks.findByName("genSources")?.dependsOn("genSourcesWithVineflower")
     }
 }
